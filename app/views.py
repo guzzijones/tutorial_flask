3b8a4647a2bf6196a6aff23366eb3bb45ac44852
Jonen <AXJ0NWL@fpl.com>
2018-03-03 19:47:17 -0500

    login

--- app/views.py
+++ app/views.py
@@ -1,11 +1,33 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname':'aj'}
-    return render_template('index.html',title='Home',user=user)
+    posts =[
+        {'author' :
+              {'nickname': 'John'},
+         'body':'something'
+         },
+        {
+         'author' :
+               {'nickname': 'Susan'},
+          'body':'another movie review'
+        }
+     ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
 
-
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OPENID:"%s", remember_me:%s' %
+              (form.openid.data,str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('signin.html',title='Sign In',form=form)
